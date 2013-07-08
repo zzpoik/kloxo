0e35d39779cfe35d921ab463bfdcc4314d4f0bd2
Mustafa Ramadhan <mustafa@bigraf.com>
2013-07-09 02:57:09 +0700

    add script to create mirror for mratwork/centalt

---
+++ createrepo-mratwork.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+rpm --quiet -q createrepo
+
+if [ $? != 0 ] ; then
+	yum install createrepo -q -y
+fi
+
+CURRPATH=$(cd $(dirname $0); pwd)
+
+cd $CURRPATH
+
+for type in release testing
+do
+	for ver in centos5 centos6 neutral
+	do
+		for item in i386 x86_64 noarch
+		do
+			mkdir -p $CURRPATH/repo/mratwork/$type/$ver/$item
+		done
+	done
+done
+
+mkdir -p $CURRPATH/repo/mratwork/SRPMS
+
+createrepo --database --update $CURRPATH/repo/mratwork/SRPMS
+
+for type in release testing
+do
+	for ver in centos5 centos6 neutral
+	do
+		for item in i386 x86_64 noarch
+		do
+			createrepo --checkts --update $CURRPATH/repo/mratwork/$type/$ver/$item
+		done
+	done
+done
+
+chmod -R o-w+r $CURRPATH
+
+### MR -- need change path because don't want path include '/home/repos/rpms.mratwork.com/repo/mratwork'
+cd $CURRPATH/repo/mratwork/
+
+zip -r9yD $CURRPATH/rpms-release-repodataonly.zip "./release" -x "*/*.rpm"
+zip -r9yD $CURRPATH/rpms-testing-repodataonly.zip "./testing" -x "*/*.rpm"
+zip -r9yD $CURRPATH/rpms-srpms-repodataonly.zip "./SRPMS" -x "*/*.rpm"
+
+cd $CURRPATH
---
+++ mirror-mratwork.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+rpm --quiet -q createrepo
+
+if [ $? != 0 ] ; then
+	yum install createrepo -q -y
+fi
+
+CURRPATH=$(cd $(dirname $0); pwd)
+
+cd $CURRPATH
+
+### MR -- CentAlt rpms ###
+REPOPATH="$CURRPATH/repo/mratwork"
+
+### MR -- release/testing portion ###
+for a in release testing
+do
+	for b in neutral centos5 centos6
+	do
+		for c in noarch i386 x86_64
+		do
+			if [ -d $REPOPATH/$a/$b/$c ] ; then
+				mv -f $REPOPATH/$a/$b/$c $REPOPATH/$a/$b/$a-$b-$c
+			fi
+
+			reposync --delete --newest-only \
+				--config=$CURRPATH/kloxomr-mirror.repo \
+				--repoid=$a-$b-$c --download_path=$REPOPATH/$a/$b
+
+			mv -f $REPOPATH/$a/$b/$a-$b-$c $REPOPATH/$a/$b/$c
+
+			createrepo --checkts --update $REPOPATH/$a/$b/$c
+		done
+	done
+done
+
+### MR -- SRPMS portion ###
+if yum list installed yum* | grep @ ; then
+	reposync --delete --norepopath --newest-only \
+		--config=$CURRPATH/kloxomr-mirror.repo \
+		--repoid=srpms --download_path=$REPOPATH/SRPMS
+else
+	if [ -d $REPOPATH/$a/$b/SRPMS ] ; then
+		mv -f $REPOPATH/$a/$b/SRPMS $REPOPATH/$a/$b/srpms
+	fi
+
+	reposync --delete --newest-only \
+		--config=$CURRPATH/kloxomr-mirror.repo \
+		--repoid=srpms --download_path=$REPOPATH/srpms
+
+	mv -f $REPOPATH/$a/$b/srpms $REPOPATH/$a/$b/SRPMS
+fi
+
+createrepo --checkts --update $REPOPATH/SRPMS
+
+chmod -R o-w+r $CURRPATH
+
