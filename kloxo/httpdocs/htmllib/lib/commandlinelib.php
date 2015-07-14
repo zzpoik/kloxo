e30b8158cbdb9fce79824ed366671612f691431d
mustafaramadhan <mustafa@bigraf.com>
2015-07-14 23:22:39 +0700

    mod commandlinelib.php where only permit for admin or aux

--- kloxo/httpdocs/htmllib/lib/commandlinelib.php
+++ kloxo/httpdocs/htmllib/lib/commandlinelib.php
@@ -4,18 +4,20 @@ function __cmd_desc_add($p, $parent = null)
 {
 	global $gbl, $sgbl, $login, $ghtml; 
 
+	validate_cmd($p);
 
 	if (!$parent) {
 		if (isset($p['parent-class']) && isset($p['parent-name'])) {
 			$parent = new $p['parent-class'](null, 'localhost', $p['parent-name']);
 			dprint("$parent->nname\n");
 			$parent->get();
+
 			if ($parent->dbaction === 'add') {
-				throw new lxException("parent_doesnt_exist", "nname", $p['parent-name']);
+				throw new lxException($login->getThrow("no_parent_object"), '', $p['parent-name']);
 			}
 
 			if (!$parent->checkIfSomeParent($login->getClName())) {
-				throw new lxException("you_are_not_the_owner_of_parent", "", $p['parent-name']);
+				throw new lxException($login->getThrow("not_owner_of_parent_object"), '', $p['parent-name']);
 			}
 
 		} else {
@@ -28,7 +30,6 @@ function __cmd_desc_add($p, $parent = null)
 
 	$var = get_variable($p);
 
-
 	if (isset($p['count'])) {
 		$oldname = $p['name'];
 		for($i = 0; $i < $p['count']; $i++) {
@@ -37,75 +38,76 @@ function __cmd_desc_add($p, $parent = null)
 			} else {
 				$p['name'] = "$oldname$i";
 			}
-			$param = exec_class_method($class, "addCommand", $parent, $class, $p);
+
+			$p = exec_class_method($class, "addCommand", $parent, $class, $p);
 			unset($var['template-name']);
-			$param = lx_array_merge(array($param, $var));
-			do_desc_add($parent, $class, $param);
+			$p = lx_array_merge(array($p, $var));
+
+			do_desc_add($parent, $class, $p);
 		}
+
 		$parent->was();
+
 		exit;
 	}
 
-
-	$param = exec_class_method($class, "addCommand", $parent, $class, $p);
+	$p = exec_class_method($class, "addCommand", $parent, $class, $p);
 
 	unset($var['template-name']);
-	$param = lx_array_merge(array($param, $var));
-	do_desc_add($parent, $class, $param);
-	$parent->was();
+	$p = lx_array_merge(array($p, $var));
+	do_desc_add($parent, $class, $p);
 
+	$parent->was();
 }
 
 function __cmd_desc_delete($p)
 {
 	global $gbl, $sgbl, $login, $ghtml; 
-	/*
-	if (isset($p['parent-class']) && isset($p['parent-name'])) {
-		$parent = new $p['parent-class'](null, 'localhost', $p['parent-name']);
-		$parent->get();
-		if ($parent->dbaction === 'add') {
-			throw new lxException("parent_doesnt_exist", "nname", $class);
-		}
-	} else {
-		$parent = $login;
-	}
-*/
+
+	validate_cmd($p);
+
 	$class = $p['class'];
 	$name = $p['name'];
 
 	$object = new $class(null, 'localhost', $name);
 	$object->get();
+
 	if ($object->dbaction === 'add') {
-		throw new lxException('object_doesnt_exist', '', $name);
+		throw new lxException($login->getThrow('no_object'), '', $name);
 	}
 
 	if (!$object->checkIfSomeParent($login->getClName())) {
-		throw new lxException("the_object_doesnt_exist_under_you", "", $object->nname);
+		throw new lxException($login->getThrow("no_object_under_current_user"), '', $object->nname);
 	}
+
 	do_desc_delete_single($object);
+
 	$object->was();
 }
-
 
 function __cmd_desc_simplelist($p)
 {
 	global $gbl, $sgbl, $login, $ghtml; 
 
+	validate_cmd($p);
+
 	ob_start();
 	$resource = $p['resource'];
 
 	$parent = null;
+
 	if (!$parent) {
 		if (isset($p['parent-class']) && isset($p['parent-name'])) {
 			$parent = new $p['parent-class'](null, 'localhost', $p['parent-name']);
 			dprint($parent->nname);
 			$parent->get();
+
 			if ($parent->dbaction === 'add') {
-				throw new lxException("parent_doesnt_exist", "nname", $p['parent-name']);
+				throw new lxException($login->getThrow("no_parent_object"), '', $p['parent-name']);
 			}
 
 			if (!$parent->checkIfSomeParent($login->getClName())) {
-				throw new lxException("you_are_not_the_owner_of_parent", "", $p['parent-name']);
+				throw new lxException($login->getThrow("not_owner_of_parent_object"), '', $p['parent-name']);
 			}
 
 		} else {
@@ -113,11 +115,9 @@ function __cmd_desc_simplelist($p)
 		}
 	}
 
-
 	$list = $parent->getCommandResource($resource);
 
 	if (!$list) {
-
 		// Fix for WHMCS needing pserver in client.
 
 		if (!$parent->isAdmin() && $sgbl->isKloxo() && $resource === 'pserver') {
@@ -126,20 +126,25 @@ function __cmd_desc_simplelist($p)
 		}
 
 		$list = $parent->getList($resource);
+
 		if (isset($p['v-filter'])) {
 			list($var, $val) = explode(":", $p['v-filter']);
+
 			foreach($list as $k => $l) {
 				if ($l->$var !== $val) {
 					unset($list[$k]);
 				}
 			}
 		}
+
 		if (!$list) {
 			json_print("error", $p, "__error_no_resource_for_$resource");
 			exit;
 		}
+
 		$list = get_namelist_from_objectlist($list, "nname", "nname");
 	}
+
 	ob_end_clean();
 
 	return $list;
@@ -152,20 +157,22 @@ function copy_nname_to_name(&$p)
 	}
 }
 
-
 function __cmd_desc_update($p)
 {
-	global $gbl, $sgbl, $login, $ghtml; 
+	global $gbl, $sgbl, $login, $ghtml;
+
+	validate_cmd($p);
+
 	copy_nname_to_name($p);
 	$object = new $p['class'](null, 'localhost', $p['name']);
 	$object->get();
 
 	if ($object->dbaction === 'add') {
-		throw new lxException("object_doesnt_exist", "name", $p['name']);
+		throw new lxException($login->getThrow("no_object"), '', $p['name']);
 	}
 
 	if (!$object->checkIfSomeParent($login->getClName())) {
-		throw new lxException("the_object_doesnt_exist_under_you", "", $object->nname);
+		throw new lxException($login->getThrow("no_object_under_current_user"), '', $object->nname);
 	}
 
 	$tparam = get_variable($p);
@@ -173,41 +180,49 @@ function __cmd_desc_update($p)
 
 	$tparam = $object->commandUpdate($subaction, $tparam);
 
-	$param = array();
+	$p = array();
+
 	foreach($tparam as $k => $v) {
 		$k = str_replace("-", "_s_", $k);
-		$param[$k] = $v;
+		$p[$k] = $v;
 	}
-	dprintr($param);
-	do_desc_update($object, $subaction, $param);
+
+	dprintr($p);
+	do_desc_update($object, $subaction, $p);
+
 	$object->was();
 }
 
-function __cmd_desc_getproperty($param)
+function __cmd_desc_getproperty($p)
 {
 	global $gbl, $sgbl, $login, $ghtml; 
-	if (isset($param['name']) && isset($param['class'])) {
-		$name = $param['name'];
-		$class = $param['class'];
+
+	validate_cmd($p);
+
+	if (isset($p['name']) && isset($p['class'])) {
+		$name = $p['name'];
+		$class = $p['class'];
 		$object = new $class(null, 'localhost', $name);
 		$object->get();
+
 		if ($object->dbaction === 'add') {
-			throw new lxException('object_doesnt_exist', 'name', $name);
+			throw new lxException($login->getThrow('no_object'), '', $name);
 		}
 	} else {
 		$object = $login;
 	}
-
-
+ 
 	$object->getHardProperty();
 
-	$vlist = get_variable($param);
+	$vlist = get_variable($p);
 
 	foreach($vlist as $k => $v) {
 		$nv = $k;
+
 		if (csa($nv, "-")) {
 			$cc = explode("-", $nv);
 			$result["v-$k"] = $object->{$cc[0]}->$cc[1];
+
 			continue;
 		}
 
@@ -217,10 +232,25 @@ function __cmd_desc_getproperty($param)
 					$result["v-$nv-$kk"] =  $nnv;
 				}
 			}
+
 			continue;
 		}
+
 		$result["v-$nv"] =  $object->$nv;
 	}
+
 	return $result;
 }
 
+function validate_cmd($p)
+{
+	global $gbl, $sgbl, $login, $ghtml; 
+
+	if (($p['login-class'] === 'client') && ($p['login-name'] === 'admin')) {
+		//
+	} elseif (($p['login-class'] === 'auxiliary') && (stripos($p['login-name'], '.aux') !== false)) {
+		//
+	} else {
+		throw new lxException($login->getThrow('only_permit_for_admin_or_auxiliary_login'), '', $p['login-name']);	
+	}
+}
